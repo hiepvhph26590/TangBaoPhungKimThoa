2323c8f870d584648bb56e482e2b904a486bd1d7
hiepvhph26590 <hiepvhph26590@fpt.edu.vn>
2023-05-14 02:40:06 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// BEST VIEWED IN FULL-SCREEN ON CHROME / SAFARI 
+ type="text/javascript"
+// Đợi 3 giây và chuyển trang
+setTimeout(function(){
+    window.location.href = "https://hiepvhph26590.github.io/tangBao/"; // Thay đổi URL của trang mới ở đây
+}, 5000);
+
+// Free from JS
